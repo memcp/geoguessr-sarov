19ec4862cf3b96e06cec7a06704b612562c566dc
ilyas Garipov <ilyasgaripov97@gmail.com>
2022-06-25 22:35:21 +0300

    Add full paths

--- src/places.js
+++ src/places.js
@@ -3,18 +3,18 @@ var places = [
     lat: 54.921682,
     lng: 43.348060,
     address: 'Шевченко 30',
-    href: "../assets/home.jpg"
+    href: "https://ilyasgaripov.ru/projects/geoguessr-sarov/assets/home.jpg"
   },
   {
     lat: 54.92253019652477,
     lng: 43.350409269332886,
     name: 'Other Place',
-    href: "../assets/mc.webp"
+    href: "https://ilyasgaripov.ru/projects/geoguessr-sarov/assets/mc.webp"
   },
   {
     lat: 54.91639024730667,
     lng: 43.34148824214935,
-    href: "../assets/shpil.jpg"
+    href: "https://ilyasgaripov.ru/projects/geoguessr-sarov/assets/shpil.jpg"
   }
 ];
 
