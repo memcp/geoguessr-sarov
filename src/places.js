67b47cf3e108481abdc86e3b3c12b8cf037a6aab
ilyas Garipov <ilyasgaripov97@gmail.com>
2022-06-25 22:22:34 +0300

    Use relative image paths

--- src/places.js
+++ src/places.js
@@ -3,18 +3,18 @@ var places = [
     lat: 54.921682,
     lng: 43.348060,
     address: 'Шевченко 30',
-    href: "http://www.sarov.net/p/gga.jpg"
+    href: "../assets/home.jpg"
   },
   {
     lat: 54.92253019652477,
     lng: 43.350409269332886,
     name: 'Other Place',
-    href: "https://avatars.mds.yandex.net/get-altay/4537001/2a0000017925b14cc18a56d1c559f9a1072a/XXL"
+    href: "../assets/mc.webp"
   },
   {
     lat: 54.91639024730667,
     lng: 43.34148824214935,
-    href: "https://storage.yandexcloud.net/serafim-uploads/content/2020/06/01/dom-so-shpilempr_56c52167-1fc6-4fb8-991b-ed610f16edbc.jpg"
+    href: "../assets/shpil.jpg"
   }
 ];
 
